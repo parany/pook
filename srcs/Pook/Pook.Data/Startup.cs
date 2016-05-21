b16f5da8cd02eb372044cc47cd399eed630e7f96
parany <ramilison.andriamparany@gmail.com>
2016-05-21 17:57:49 +0400

    First commit

---
+++ srcs/Pook/Pook.Data/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pook.Data.Startup))]
+namespace Pook.Data
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
