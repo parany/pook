41fb21ef103b2ddeb45781adb9603cc6d9cb963a
ANDRIANANDRAINA Andriamparany <anyparandry@gmail.com>
2016-06-14 08:32:43 +0400

    [back office] cohérence *Homepage -actuellement, la page d'acceuil est encore la page par défaut de ASP.NET MVC. Modifier cela pour que ce soit la page Book Index.

--- Pook.Web/App_Start/RouteConfig.cs
+++ Pook.Web/App_Start/RouteConfig.cs
@@ -18,7 +18,7 @@ namespace Pook.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Book", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
