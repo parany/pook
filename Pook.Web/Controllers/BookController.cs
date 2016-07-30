fa31a03240e7d3148a181125a5d67151a9faa690
ANDRIANANDRAINA Andriamparany <ramilison.andriamparany@gmail.com>
2016-07-30 19:23:14 +0400

    [Book] menu *In the "Book" menu, add submenus: -list:  -list of all books  -sort by Title  -display total count  -field:   -title   -Category   -nb of pages   -Release date   -Status:    -NA: no Status entry    -Bookmark: last status == bookmark    -Read: last status == read    -Current: last status == current    -StartRead: last status == startread   -Actions:    -Details    -Bookmark: like delete, open a new page for GET and display the details, before deleting. add this button for these book that are not bookmarked.

--- Pook.Web/Controllers/BookController.cs
+++ Pook.Web/Controllers/BookController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using Pook.Data.Entities;
 using Pook.Data.Repositories.Interface;
 using Pook.Web.Models;
@@ -20,16 +22,21 @@ namespace Pook.Web.Controllers
 
         private IGenericRepository<Firm> FirmRepository { get; }
 
-
         private IGenericRepository<Editor> EditorRepository { get; }
+
+        private IGenericRepository<Progression> ProgressionRepository { get; }
+
+        private IGenericRepository<Status> StatusRepository { get; }
 
 
         public BookController(
-            IGenericRepository<Book> bookRepository, 
+            IGenericRepository<Book> bookRepository,
             IGenericRepository<Responsability> responsabilityRepository,
             IGenericRepository<Note> noteRepository,
             IGenericRepository<Category> categoryRepository,
             IGenericRepository<Firm> firmRepository,
+            IGenericRepository<Progression> progressionRepository,
+            IGenericRepository<Status> statusRepository,
             IGenericRepository<Editor> editorRepository
             )
         {
@@ -39,17 +46,22 @@ namespace Pook.Web.Controllers
             CategoryRepository = categoryRepository;
             FirmRepository = firmRepository;
             EditorRepository = editorRepository;
+            StatusRepository = statusRepository;
+            ProgressionRepository = progressionRepository;
 
             BookRepository.AddNavigationProperties(
                 b => b.Category,
                 b => b.Editor,
                 b => b.Firm
                 );
+            BookRepository.SetSortExpression(l => l.OrderBy(b => b.Title));
             ResponsabilityRepository.AddNavigationProperties(
                 r => r.Author,
                 r => r.ResponsabilityType
                 );
             NoteRepository.AddNavigationProperty(u => u.User);
+            ProgressionRepository.AddNavigationProperty(p => p.Status);
+            ProgressionRepository.SetSortExpression(l => l.OrderByDescending(p => p.Date));
         }
 
         // GET: Book
@@ -58,6 +70,34 @@ namespace Pook.Web.Controllers
             var books = BookRepository.GetAll();
             books = books.OrderBy(b => b.Title).ToList();
             return View(books.ToList());
+        }
+
+        [Route("Book/List")]
+        public ViewResult List()
+        {
+            BookRepository.AddIgnoreProperty(b => b.Progressions);
+            var books = BookRepository.GetAll();
+            var userId = User.Identity.GetUserId();
+            var progressions = ProgressionRepository.GetList(b => b.UserId == userId);
+            progressions =
+                (from progression in progressions
+                 group progression by progression.BookId
+                 into g
+                 select g.First()
+                 ).ToList();
+            var bookModels =
+                (from book in books
+                 let progression = progressions.FirstOrDefault(p => p.BookId == book.Id)
+                 select new BookList
+                 {
+                     Id = book.Id,
+                     Status = progression != null ? progression.Status.Title : "N/A",
+                     Title = book.Title,
+                     Category = book.Category.Title,
+                     NumberOfPages = book.NumberOfPages,
+                     ReleaseDate = book.ReleaseDate
+                 }).ToList();
+            return View(bookModels);
         }
 
         [Route("Book/Details/{id}")]
@@ -120,14 +160,14 @@ namespace Pook.Web.Controllers
         public ActionResult Edit(Guid? id)
         {
             if (id == null)
-            
+
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            
+
             Book book = BookRepository.GetSingle(id.Value);
 
             if (book == null)
                 return HttpNotFound();
-            
+
             ViewBag.CategoryId = new SelectList(CategoryRepository.GetAll(), "Id", "Title", book.CategoryId);
             var allEditors = EditorRepository.GetAll();
             allEditors.Insert(0, null);
@@ -167,7 +207,7 @@ namespace Pook.Web.Controllers
             Book book = BookRepository.GetSingle(id.Value);
             if (book == null)
                 return HttpNotFound();
-            
+
             return View(book);
         }
 
@@ -178,6 +218,36 @@ namespace Pook.Web.Controllers
         {
             BookRepository.Delete(id);
             return RedirectToAction("Index");
+        }
+
+        // GET: Book/Bookmark/5
+        public ActionResult Bookmark(Guid? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Book book = BookRepository.GetSingle(id.Value);
+            if (book == null)
+                return HttpNotFound();
+
+            return View(book);
+        }
+
+        // POST: Book/Bookmark/5
+        [HttpPost, ActionName("Bookmark")]
+        [ValidateAntiForgeryToken]
+        public ActionResult BookmarkConfirmed(Guid id)
+        {
+            var bookmarkStatus = StatusRepository.GetSingle(s => s.Title == "Bookmarked");
+            var progression = new Progression
+            {
+                BookId = id,
+                StatusId = bookmarkStatus.Id,
+                UserId = User.Identity.GetUserId(),
+                Date = DateTime.Now
+            };
+            ProgressionRepository.Add(progression);
+            return RedirectToAction("List");
         }
     }
 }
---
+++ Pook.Web/Models/BookList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pook.Web.Models
+{
+    public class BookList
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public int NumberOfPages { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}")]
+        public DateTime ReleaseDate { get; set; }
+
+        public string Status { get; set; }
+    }
+}
