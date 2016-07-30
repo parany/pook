25475075a93ebef7d76847c068e30259a96ed06f
ANDRIANANDRAINA Andriamparany <ramilison.andriamparany@gmail.com>
2016-07-30 13:49:31 +0400

    But remove all references to the dbcontext in the web project. To achieve this, implement the Repository Pattern and use DI to inject all necessary repository.

--- Pook.Web/Controllers/StatusController.cs
+++ Pook.Web/Controllers/StatusController.cs
@@ -1,39 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using Pook.Data;
 using Pook.Data.Entities;
+using Pook.Data.Repositories.Interface;
 
 namespace Pook.Web.Controllers
 {
     public class StatusController : Controller
     {
-        private PookDbContext db = new PookDbContext();
+        private IGenericRepository<Status> StatusRepository { get; }
+
+        public StatusController(IGenericRepository<Status> statusRepository)
+        {
+            StatusRepository = statusRepository;
+        }
 
         // GET: Status
         public ActionResult Index()
         {
-            return View(db.Statuses.ToList());
-        }
-
-        // GET: Status/Details/5
-        public ActionResult Details(Guid? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Status status = db.Statuses.Find(id);
-            if (status == null)
-            {
-                return HttpNotFound();
-            }
-            return View(status);
+            return View(StatusRepository.GetAll());
         }
 
         // GET: Status/Create
@@ -43,17 +28,13 @@ namespace Pook.Web.Controllers
         }
 
         // POST: Status/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Status status)
         {
             if (ModelState.IsValid)
             {
-                status.Id = Guid.NewGuid();
-                db.Statuses.Add(status);
-                db.SaveChanges();
+                StatusRepository.Add(status);
                 return RedirectToAction("Index");
             }
 
@@ -67,7 +48,7 @@ namespace Pook.Web.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Status status = db.Statuses.Find(id);
+            Status status = StatusRepository.GetSingle(id.Value);
             if (status == null)
             {
                 return HttpNotFound();
@@ -76,54 +57,16 @@ namespace Pook.Web.Controllers
         }
 
         // POST: Status/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "StatusId,Title,CreatedOn,UpdatedOn,CreatedBy,UpdatedBy,SeoTitle")] Status status)
+        public ActionResult Edit(Status status)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(status).State = EntityState.Modified;
-                db.SaveChanges();
+                StatusRepository.Update(status);
                 return RedirectToAction("Index");
             }
             return View(status);
-        }
-
-        // GET: Status/Delete/5
-        public ActionResult Delete(Guid? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Status status = db.Statuses.Find(id);
-            if (status == null)
-            {
-                return HttpNotFound();
-            }
-            return View(status);
-        }
-
-        // POST: Status/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(Guid id)
-        {
-            Status status = db.Statuses.Find(id);
-            db.Statuses.Remove(status);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
