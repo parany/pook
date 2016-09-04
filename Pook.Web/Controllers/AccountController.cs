a46a639389bb5f6d176dafb1f6ab712da6d5a149
ANDRIANANDRAINA Andriamparany <ramilison.andriamparany@gmail.com>
2016-09-04 11:47:37 +0400

    Removed HomeController

--- Pook.Web/Controllers/AccountController.cs
+++ Pook.Web/Controllers/AccountController.cs
@@ -175,7 +175,7 @@ namespace Pook.Web.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Book");
                 }
                 AddErrors(result);
             }
@@ -399,7 +399,7 @@ namespace Pook.Web.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login");
         }
 
         //
@@ -439,7 +439,7 @@ namespace Pook.Web.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Book");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
